8eda5846158e6d7818b815998e0bf4a196ae2813
ishubarman7 <ishu.barman9067900@gmail.com>
2024-08-11 22:23:37 +0530

    Create add.cpp

---
+++ OOPs/add.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+class add
+{
+    public:
+    int a,b,c;
+    
+    void addition()
+    {
+    c = a + b;
+    std::cout<<"Addition of two numbers a and b are equal to: "<<c<<std::endl;
+    }
+};
+
+int main()
+{
+    add A1;
+    A1.a, A1.b;
+    std::cout<<"Enter the Two Numbersa a and b: "<<std::endl;
+    std::cin>>A1.a;
+    std::cin>>A1.b;
+
+    A1.addition();
+    return 0;
+
+}
