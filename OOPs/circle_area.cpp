812127d7d7f299cab81ec2f364b177c0febed536
ishubarman7 <ishu.barman9067900@gmail.com>
2024-08-11 22:09:54 +0530

    Create circle_area.cpp

---
+++ OOPs/circle_area.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+#define pi 3.14
+
+class abc
+{   
+    public:
+
+    int r, area;
+
+    void calculation()
+    {
+        area = r*r*pi;
+        cout<<"Area of the Circle is: "<<area<<endl;
+    }
+};
+
+int main()
+{
+    abc A;
+    A.r;
+    cout<<"Enter the Radius of the Circle: ";
+    cin >> A.r;
+
+    A.calculation();
+    return 0;
+
+}
