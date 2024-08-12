b4a1c50c7906615138a507874825b1ecf2175f63
ishubarman7 <ishu.barman9067900@gmail.com>
2024-08-12 10:49:11 +0000

    first lab code

---
+++ OOPs/firstlab.cpp
@@ -0,0 +1,24 @@
+#inlude<iostream>
+using namespace std;
+
+class add
+{
+    private:
+    int a, b, c;
+
+    public:
+    void sum()
+    {
+        cout<<"Enter any two numbers: "<<endl;
+        cin>>a>>b;
+        c = a + b;
+        cout<<"Sum of these two no. are: ";
+    }
+};
+
+int main()
+{
+    add A;
+    A.sum();
+    return 0;
+}
