4ee96eba356e1033bfcc90545bb18648078c241f
ishubarman7 <ishu.barman9067900@gmail.com>
2024-08-14 23:33:34 +0530

    Update 14-8-24_binary-search.cpp

--- DSA/14-8-24_binary-search.cpp
+++ DSA/14-8-24_binary-search.cpp
@@ -22,7 +22,7 @@ int binarysearch(int array[], int left, int right, int x)
 
 int main()
 {
-    int array[] = {2,5,8,9,10,18,19,21,25,26,27,29};
+    int array[] = {2,5,8,9,10,18,19,21,25,26,29};
     int x = 25;
     int left = 0;
     int n = sizeof(array) / sizeof(array[0]);
